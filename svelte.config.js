5cdf17b2ed853a95b6769e801b5df2567629584f
Christophe R Patraldo <patrouch@gmail.com>
2022-01-13 13:35:44 -0500

    npm run build & preview don't give errors after initializing firebase config

--- svelte.config.js
+++ svelte.config.js
@@ -1,11 +1,38 @@
 import adapter from '@sveltejs/adapter-node';
 import preprocess from 'svelte-preprocess';
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+//import { getAnalytics } from "firebase/analytics";
+
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyBymINyZcWd_0mWmz2dqPoMPI-Io65OzQs",
+  authDomain: "sveltekitauth007.firebaseapp.com",
+  projectId: "sveltekitauth007",
+  storageBucket: "sveltekitauth007.appspot.com",
+  messagingSenderId: "705326568792",
+  appId: "1:705326568792:web:ac257ecca558bf1767c12d",
+  measurementId: "G-E1463GMFE9"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+//const analytics = getAnalytics(app);
+
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	// Consult https://github.com/sveltejs/svelte-preprocess
 	// for more information about preprocessors
-	preprocess: preprocess(),
+	preprocess: [
+		preprocess({
+		postcss: false 
+		}),
+	],
 
 	kit: {
 		adapter: adapter({
