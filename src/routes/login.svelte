35f8f2bd9cd5a712e9a69b0c85d128406cb44030
Christophe R Patraldo <patrouch@gmail.com>
2022-01-18 09:29:04 -0500

    added login button that triggers signInWithPopup

--- src/routes/login.svelte
+++ src/routes/login.svelte
@@ -1,7 +1,7 @@
 <script lang='ts'>
 
 import { initializeApp } from "firebase/app";
-import { getAuth, GoogleAuthProvider, onAuthStateChanged, signInWithPopup, signInWithRedirect } from "firebase/auth";
+import { getAuth, getRedirectResult, GoogleAuthProvider, onAuthStateChanged, signInWithPopup, signInWithRedirect } from "firebase/auth";
 const firebaseConfig = {
     apiKey: "AIzaSyBymINyZcWd_0mWmz2dqPoMPI-Io65OzQs",
     authDomain: "sveltekitauth007.firebaseapp.com",
@@ -18,8 +18,22 @@ const provider = new GoogleAuthProvider();
 provider.addScope('profile');
 provider.addScope('email');
 const login = async () => {
-  signInWithPopup(auth, provider);
-}
+    //try {
+      await signInWithRedirect(auth,provider);
+  //signInWithPopup(auth, provider);
+  //signInWithRedirect(auth, provider);
+// After returning from the redirect when your app initializes you can obtain the result
+const result = await getRedirectResult(auth);
+   // } //catch (result){
+console.error();
+if (result) {
+  // This is the signed-in user
+  const user = result.user;
+  // This gives you a Google Access Token.
+  const credential = GoogleAuthProvider.credentialFromResult(result);
+  const token = credential.accessToken;
+    }
+    };
 console.log(auth)
 </script>
 <h1>Login</h1>
